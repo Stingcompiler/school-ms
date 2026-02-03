a0602014a398776a63dc70dec82936540f8d218c
Stingcompiler <108219321+Stingcompiler@users.noreply.github.com>
2026-02-04 00:20:08 +0200

    Update build script to skip frontend build
    Removed frontend build steps and added deployment echo.

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Build Frontend
-cd frontend
-npm install
-npm run build
-cd ..
+echo "Deploying..."
 
-# Setup Backend
+# 1. Skip Frontend Build (You already did this locally!)
+
+# 2. Go into the backend folder
 cd backend
 
-# Install Python requirements
+# 3. Install Python requirements
 pip install -r requirements.txt
 
-# Collect static files (Images + React build)
+# 4. Collect static files
+# (This will take your uploaded 'frontend_build' and move it to 'staticfiles')
 python manage.py collectstatic --noinput
 
-# Run DB migrations
+# 5. Run DB migrations
 python manage.py migrate
